fdfe498e89654fa3e5e2ea37417caf05d5298949
김태형 <taehyeong224@gmail.com>
2020-03-31 09:10:50 +0900

    init2

---
+++ src/config/token.ts
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+export const channels = {
+    general: "C0110LQNXJ4"
+}
+
+export const token: string | undefined = process.env.TOKEN;
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {WebClient} from "@slack/web-api";
+import {RTMClient} from "@slack/rtm-api";
+import { token, channels } from "./config/token";
+console.log("token: ", token)
+const rtm = new RTMClient(token || "");
+const web = new WebClient(token);
+rtm.start().then(() => {
+    console.log("start")
+    rtm.on('ready', () => {
+        console.log("rtm ready");
+        rtm.on('message', async (message: {subtype?: string, text: string}) => {
+            const {subtype, text} = message;
+            if (subtype !== undefined && subtype === 'bot_message') {
+                return;
+            }
+        
+            if (text === "바보") {
+                const payload = {channel: channels.general, text: "반사", icon_emoji: ":raised_hand_with_fingers_splayed:"};
+                web.chat.postMessage(payload);
+            }
+
+            if (text.startsWith("맛집")) {
+                const splited: string[] = text.split(" ");
+                const query = encodeURIComponent(`${splited.join(" ")} 맛집`)
+                const payload = {channel: channels.general, text: `https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${query}`, icon_emoji: ":ice_cream:"};
+                web.chat.postMessage(payload);
+            }
+        });
+    });
+    
+}).catch(console.error);
+
+
