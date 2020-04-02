3df1d313d58fbc5c55e9045ebd53b485e38903ae
kimtaehyeong <taehyeong224@gmail.com>
2020-04-02 13:39:21 +0900

    edit bug

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,25 @@ rtm.start().then(() => {
     console.log("start")
     rtm.on('ready', () => {
         console.log("rtm ready");
-        rtm.on('message', async (message: {subtype?: string, text: string}) => {
-            const {subtype, text} = message;
-            if (subtype !== undefined && subtype === 'bot_message') {
-                return;
-            }
-        
-            if (text === "바보") {
-                const payload = {channel: channels.general, text: "반사", icon_emoji: ":raised_hand_with_fingers_splayed:"};
-                web.chat.postMessage(payload);
-            }
+    });
+    rtm.on('message', async (message: {subtype?: string, text: string}) => {
+        const {subtype, text} = message;
+        if (subtype !== undefined && subtype === 'bot_message') {
+            return;
+        }
 
-            if (text.startsWith("맛집")) {
-                const splited: string[] = text.split(" ");
-                splited.splice(0, 1);
-                const query = encodeURIComponent(`${splited.join(" ")} 맛집`)
-                const payload = {channel: channels.general, text: `https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${query}`, icon_emoji: ":ice_cream:"};
-                web.chat.postMessage(payload);
-            }
-        });
+        if (text === "바보") {
+            const payload = {channel: channels.general, text: "반사", icon_emoji: ":raised_hand_with_fingers_splayed:"};
+            web.chat.postMessage(payload);
+        }
+
+        if (text.startsWith("맛집")) {
+            const splited: string[] = text.split(" ");
+            splited.splice(0, 1);
+            const query = encodeURIComponent(`${splited.join(" ")} 맛집`)
+            const payload = {channel: channels.general, text: `https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${query}`, icon_emoji: ":ice_cream:"};
+            web.chat.postMessage(payload);
+        }
     });
     
 }).catch(console.error);
