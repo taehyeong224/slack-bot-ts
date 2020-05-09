786a5139a5b10c1149565b030a03ee8e02a0ef71
김태형 <taehyeong224@gmail.com>
2020-05-09 09:42:00 +0900

    월~금

--- src/controller/cron/Challenge.ts
+++ src/controller/cron/Challenge.ts
@@ -4,7 +4,7 @@ import {channels} from "../../config/token";
 import {BaseCron} from "./base/BaseCron";
 
 export class Challenge extends BaseCron {
-    private readonly rule: string = "0 10-18 * * *"; // 매일 10시 ~ 18 시 매 시간 마다
+    private readonly rule: string = "0 10-18 * * 1-5"; // 월~금 10시 ~ 18 시 매 시간 마다
     // private readonly rule: string = "* * * * *"; // 1분 마다
 
     constructor(webClient: WebClient) {
