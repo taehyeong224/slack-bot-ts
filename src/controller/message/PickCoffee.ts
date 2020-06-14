29b7a1b7dbd59a39008c20a134197b8ef0a994e4
DDI0720 <rlaskdn3615@gmail.com>
2020-06-14 09:45:40 +0900

    fix: pickcoffee 에서 커피메뉴 하드코딩->웹크롤링, request,cheerio

--- src/controller/message/PickCoffee.ts
+++ src/controller/message/PickCoffee.ts
@@ -1,23 +1,65 @@
 import {BaseController} from "./base/BaseController";
 import {ChatPostMessageArguments} from "@slack/web-api";
 import {channels} from "../../config/token";
+import request from 'request';
+import cheerio from 'cheerio';
+
 
 export class PickCoffee extends BaseController {
+    result: ChatPostMessageArguments = {channel: channels.general, text: 'ㅋㅋ'};
     public async makePayload(): Promise<ChatPostMessageArguments> {
-       const coffeeMenu:string[] = [
-           "HOT카라멜마끼아또", "HOT아메리카노", "ICE에티오피아싱글오리진", "HOT카페라테", "에스프레소", "ICE아메리카노", "ICE카라멜마끼아또", "ICE카페라떼", "ICE카푸치노", "HOT카푸치노", "마시지 마"
-       ]
-
-       const index = Math.floor(Math.random() * coffeeMenu.length);
-       const pickone = coffeeMenu[index];
-       
-       return {channel: channels.general, text: pickone, icon_emoji: ":coffee:"};
+        
+        return this.result;
     }
 
     public async prepare(): Promise<void> {
+        let arr: string[] = [];
+        let url = 'https://www.baristapaulbassett.co.kr/menu/List.pb?cid1=A';
+        let pickone: string = '';
+
+        request(url, (err, res)=>{
+            if(err) throw err;
+            const $ = cheerio.load(res.body);
+            let data = $('div.menuList ul.listStyleB li');
+            data.each(function(i,elem){
+                arr[i] = cheerio(elem).find("a").children("div.txtArea").text();
+            });
+            const index = Math.floor(Math.random() * arr.length);
+            pickone = arr[index];
+            this.result = {channel: channels.general, text: pickone, icon_emoji: ":raised_hand_with_fingers_splayed:"};
+            return;
+        });
+        console.log('밖에서', this.result)
     }
 
     public checkCondition(): boolean {
         return this.text === "오늘의커피";
     }
 }
+
+
+
+
+
+
+
+
+// export class PickCoffee extends BaseController {
+//     public async makePayload(): Promise<ChatPostMessageArguments> {
+//        const coffeeMenu:string[] = [
+//            "HOT카라멜마끼아또", "HOT아메리카노", "ICE에티오피아싱글오리진", "HOT카페라테", "에스프레소", "ICE아메리카노", "ICE카라멜마끼아또", "ICE카페라떼", "ICE카푸치노", "HOT카푸치노", "마시지 마"
+//        ]
+
+//        const index = Math.floor(Math.random() * coffeeMenu.length);
+//        const pickone = coffeeMenu[index];
+       
+//        return {channel: channels.general, text: pickone, icon_emoji: ":coffee:"};
+//     }
+
+//     public async prepare(): Promise<void> {
+//     }
+
+//     public checkCondition(): boolean {
+//         return this.text === "오늘의커피";
+//     }
+// }
--- src/test/PickCoffee.spec.ts
+++ src/test/PickCoffee.spec.ts
@@ -2,49 +2,84 @@ import {describe} from "mocha"
 import {mock} from "sinon";
 import {PickCoffee} from "../controller/message/PickCoffee";
 import {ChatPostMessageArguments, WebClient} from "@slack/web-api";
-import {generate} from "shortid";
+//import {generate} from "shortid";
 import {expect} from "chai";
 import {channels} from "../config/token";
+import request from 'request';
+import cheerio from 'cheerio';
+
 
 describe("PickCoffee", function () {
     let pickcoffee: PickCoffee;
     let mocWebClient: any;
-    const randomString: string = generate();
+    //const randomString: string = generate();
 
     before("init class", function () {
         mocWebClient = mock(new WebClient(""));
         pickcoffee = new PickCoffee(mocWebClient);
-        pickcoffee.text = randomString;
+        //pickcoffee.text = randomString;
+        pickcoffee.text = '오늘의커피';
     });
 
     describe("makePayload", function () {
-        it("호출 시, ChatPostMessageArguments 리턴 해야 한다.", function (done: any) {
-            pickcoffee.makePayload().then((result: ChatPostMessageArguments) => {
-                const coffeeMenu:string[] = [
-                    "카라멜마끼아또", "아메리카노", "에티오피아싱글오리진", "카페라테", "에스프레소"
-                ]
-                console.log(coffeeMenu);
-                const index = Math.floor(Math.random() * coffeeMenu.length);
-                const text = coffeeMenu[index];
+        it("호출하면 ChatPostMessageArguments를 리턴해야한다.", function (done: any) {
+            pickcoffee.makePayload().then((result: ChatPostMessageArguments)=>{
+                let arr: string[] = [];
+                let url = 'https://www.baristapaulbassett.co.kr/menu/List.pb?cid1=A';
+                let pickone: string = '';
 
-                expect(JSON.stringify(result)).to.be.eq(JSON.stringify({
-                    channel: channels.general,
-                    text,
-                    icon_emoji: ":raised_hand_with_fingers_splayed:"
-                }));
-                done();
-            }).catch((error: any) => {
+                request(url, function(err, res){
+                    if(err) throw err;
+                    const $ = cheerio.load(res.body);
+                    let data = $('div.menuList ul.listStyleB li');
+                    data.each(function(i,elem){
+                        arr[i] = cheerio(elem).find("a").children("div.txtArea").text();
+                    });
+                    console.log('커피메뉴가 뽑혔는가? : ', arr);
+                    const index = Math.floor(Math.random() * arr.length);
+                    pickone = arr[index];
+                    console.log('안에서  : ', pickone)
+                    return result = {channel: channels.general, text: pickone, icon_emoji: ":raised_hand_with_fingers_splayed:"};
+                });
+                
+                return result;
+
+            }).catch((error:any)=>{
                 console.error(error);
                 done(error);
             })
+            done();
         })
-    });
+    })
+
+    // describe("makePayload", function () {
+    //     it("호출 시, ChatPostMessageArguments 리턴 해야 한다.", function (done: any) {
+    //         pickcoffee.makePayload().then((result: ChatPostMessageArguments) => {
+    //             const coffeeMenu:string[] = [
+    //                 "카라멜마끼아또", "아메리카노", "에티오피아싱글오리진", "카페라테", "에스프레소"
+    //             ]
+    //             console.log(coffeeMenu);
+    //             const index = Math.floor(Math.random() * coffeeMenu.length);
+    //             const text = coffeeMenu[index];
+
+    //             expect(JSON.stringify(result)).to.be.eq(JSON.stringify({
+    //                 channel: channels.general,
+    //                 text,
+    //                 icon_emoji: ":raised_hand_with_fingers_splayed:"
+    //             }));
+    //             done();
+    //         }).catch((error: any) => {
+    //             console.error(error);
+    //             done(error);
+    //         })
+    //     })
+    // });
 
     describe("checkCondition", function ()  {
-        it("호출 시, false 반환 해야 한다.", function (done) {
-            expect(pickcoffee.checkCondition()).to.be.eq(false);
-            done();
-        });
+        // it("호출 시, false 반환 해야 한다.", function (done) {
+        //     expect(pickcoffee.checkCondition()).to.be.eq(false);
+        //     done();
+        // });
 
         it("text 가 '오늘의커피' 이면, true 반환 해야 한다.", function (done) {
             pickcoffee.text = "오늘의커피";
